3b7d8972d46582396b52a086e48bc56051824072
KalpadyutiSarka <>
2022-02-15 06:47:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#print("Hello, welcome to Networkchuck Coffee!!!!!!!!!!!!!!!!!!!!!")
+
+#name = input("What is your name?\n")
+
+#if name == "Ben":
+    #print("You are not welcome here Evil Ben!! Get Out!")
+
+#else:
+    #print("Hello " + name + ", thank you for coming in today.\n")
+
+if 2 > 3:
+  print("Yep, its true!!!")
+  print("its  still true!!")
+
+else:
+  print("Nope. not true!!!")
